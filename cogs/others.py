b988010fc1149d868c21699ddeb411b48ac07210
atree01 <anyu9898@yahoo.com>
2023-02-11 13:51:47 +0800

    add key_word

--- cogs/others.py
+++ cogs/others.py
@@ -8,12 +8,15 @@ class others(commands.Cog, description='Other commands'):
     def __init__(self, bot):
         self.bot = bot
         
+        self.key_word =["@345922184017084427", "施奕安", "詩意安", "施亦安", "施易安"]
+        
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author != self.bot.user.name:
-            if '@345922184017084427' in message.content or '施奕安' in message.content:
-                path = random.choice(os.listdir('C:/Users/anyu9/OneDrive - 國立中央大學/Pixiv_imgs/'))
-                await message.channel.send(file=discord.File(f'C:/Users/anyu9/OneDrive - 國立中央大學/Pixiv_imgs/{path}'))
+            for key in self.key_word:
+                if key in message.content:
+                    path = random.choice(os.listdir('C:/Users/anyu9/OneDrive - 國立中央大學/Pixiv_imgs/'))
+                    await message.channel.send(file=discord.File(f'C:/Users/anyu9/OneDrive - 國立中央大學/Pixiv_imgs/{path}'))
         
     @app_commands.command(name='hello', description='Say hello to you.')
     async def hello(self, interaction: discord.Interaction):
